b32bf9d6bc1e0f8e5568037c4a997f50c35d21e0
Adham Muhammad <adhamfarid1110@gmail.com>
2025-01-07 14:25:40 +0700

    chore: integrate cloudinary

---
+++ pages/api/the-journey.js
@@ -0,0 +1,25 @@
+// /pages/api/get-images.js
+import { v2 as cloudinary } from "cloudinary";
+
+// Configure Cloudinary with environment variables
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default async function handler(req, res) {
+    try {
+        const { resources } = await cloudinary.search
+            .expression("folder:the-journey/*") // Replace 'your-folder-name' with your folder's name
+            .sort_by("public_id", "desc") // Sort images by most recent
+            .max_results(20) // Limit to 20 images
+            .execute();
+
+        // Map resources to return only the URLs
+        const imageUrls = resources.map((img) => img.secure_url);
+        res.status(200).json(imageUrls);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
--- pages/components/works/the-journey.js
+++ pages/components/works/the-journey.js
@@ -1,29 +1,24 @@
 "use client";
+import { useEffect, useState } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 export default function TheJourney() {
-    const images = [
-        "https://images.unsplash.com/photo-1593642632823-8f785ba67e45",
-        "https://images.unsplash.com/photo-1519125323398-675f0ddb6308",
-        "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
-        "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
-        "https://images.unsplash.com/photo-1542300057-1705d0217213",
-        "https://images.unsplash.com/photo-1494173853739-c21f58b16055",
-        "https://images.unsplash.com/photo-1477959858617-67f85cf4f1df",
-        "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
-        "https://images.unsplash.com/photo-1465146633011-14f8e85f06f2",
-        "https://images.unsplash.com/photo-1527549993586-dff825b37782",
-        "https://images.unsplash.com/photo-1550418290-a8d86ad674cd",
-        "https://images.unsplash.com/photo-1564866657315-3934f1f10cfb",
-        "https://images.unsplash.com/photo-1518081444512-371ec4a18765",
-        "https://images.unsplash.com/photo-1534515722748-df22769c1eb3",
-        "https://images.unsplash.com/photo-1558980664-10b26ac1ab2e",
-        "https://images.unsplash.com/photo-1510915228340-29c85a43dcfe",
-        "https://images.unsplash.com/photo-1510915228340-29c85a43dcfe",
-        "https://images.unsplash.com/photo-1558981403-c5f9891b1e96",
-        "https://images.unsplash.com/photo-1593642532973-d31b6557fa68",
-        "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
-    ];
+    const [images, setImages] = useState([]);
+
+    // Fetch images from the Cloudinary API
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                const res = await fetch("/api/the-journey");
+                const data = await res.json();
+                setImages(data);
+            } catch (error) {
+                console.error("Error fetching images:", error);
+            }
+        };
+
+        fetchImages();
+    }, []);
 
     return (
         <section className="wrapper-the-journey">
